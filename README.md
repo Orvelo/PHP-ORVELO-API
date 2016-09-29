788598aa8a85cd6d8fbe1202069ac3ab00e7112d
tom.jolly <tom.jolly@orvelo.com>
2016-09-29 12:53:17 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
+<<<<<<< HEAD
+Orvelo API Library for PHP
+=====================================
+
+== Description
+
+Requirements:
+  PHP 5.3.x or higher [http://www.php.net/]
+  PHP Curl extension [http://www.php.net/manual/en/intro.curl.php]
+  PHP JSON extension [http://php.net/manual/en/book.json.php]
+
+Project page:
+  http://
+
+
+Report a defect or feature request here:
+  http://code.google.com/p/google-api-php-client/issues/entry
+
+Subscribe to project updates in your feed reader:
+  http://code.google.com/feeds/p/google-api-php-client/updates/basic
+
+
+== Basic Example
+  <?php
+  require_once 'path/to/src/Google_Client.php';
+  require_once 'path/to/src/contrib/apiBooksService.php';
+
+  $client = new Google_Client();
+  $service = new Google_BooksService($client);
+
+  $optParams = array('filter' => 'free-ebooks');
+  $results = $service->volumes->listVolumes('Henry David Thoreau', $optParams);
+
+  foreach ($results['items'] as $item) {
+    print($item['volumeInfo']['title'] . '<br>');
+  }
+=======
 # PHP-ORVELO-API
+>>>>>>> 7460560... Initial commit
---
+++ src/Orvelo.php
@@ -0,0 +1,228 @@
+<?php
+
+    /**
+     * Class Orvelo
+     */
+    class Orvelo
+    {
+        private $orveloURL = "https://postman.orvelo.com/parser/curl_response";
+        private $vid;
+        private $channelHash;
+        private $formName;
+        private $fields;
+
+        /**
+         * Orvelo constructor.
+         *
+         * @since 1.0
+         *
+         * @param string|null $channelHash
+         */
+        public function __construct($channelHash  = null)
+        {
+            $this->channelHash = $channelHash;
+            $this->fields      = array();
+        }
+
+        /**
+         * Adds $_REQUEST data to the fields array
+         *
+         * @since 1.0
+         * @return void
+         */
+        private function buildFromRequest()
+        {
+            foreach ($_REQUEST as $key => $value) {
+                $this->addField($key, $value);
+            }
+        }
+
+        /**
+         * Sends form data to Orvelo
+         *
+         * @since 1.0
+         *
+         * @param bool $fromRequest If true will build the fields array using $_REQUEST. If false must first addField
+         *
+         * @return bool
+         */
+        public function send($fromRequest = true)
+        {
+
+            if ($fromRequest) {
+                $this->buildFromRequest();
+            }
+            $fields = $this->getFields();
+            $this->setChannelHash($this->channelHash);
+            if (!empty($fields)) {
+                $this->setFormName($this->formName);
+                if (isset($_COOKIE['_oal'])) {
+                    $this->setVid($_COOKIE['_oal']);
+                } else {
+                    $this->setVid("NA");
+                }
+
+                $fields_string = "";
+                foreach ($this->getFields() as $key => $value) {
+                    $fields_string .= $key . '=' . $value . '&';
+                }
+                rtrim($fields_string, '&');
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, $this->getOrveloURL());
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_VERBOSE, 0);
+                curl_setopt($ch, CURLOPT_POST, count($fields));
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+                $result = curl_exec($ch);
+                curl_close($ch);
+
+                return $result;
+            }
+
+            return false;
+        }
+
+        /**
+         * @param array $fields
+         *
+         * @since 1.0
+         * @return $this
+         */
+        public function setFields(array $fields)
+        {
+            $this->fields = $fields;
+
+            return $this;
+        }
+
+        /**
+         * Returns all the fields
+         *
+         * @since 1.0
+         * @return array
+         */
+        public function getFields()
+        {
+            return $this->fields;
+        }
+
+        /**
+         * Adds a field to the fields array
+         *
+         * @param string $key field identifier
+         * @param mixed $value field value
+         *
+         * @since 1.0
+         * @return $this
+         */
+        public function addField($key, $value)
+        {
+            $this->fields[$key] = $value;
+
+            return $this;
+        }
+
+        /**
+         * Removes a field from the fields array
+         *
+         * @param string $key field identifier of the field you would like to remove
+         *
+         * @since 1.0
+         * @return $this
+         */
+        public function removeField($key)
+        {
+            unset($this->fields[$key]);
+
+            return $this;
+        }
+
+        /**
+         * @since 1.0
+         * @return string
+         */
+        public function getFormName()
+        {
+            return $this->formName;
+        }
+
+        /**
+         * @param string $formName
+         *
+         * @since 1.0
+         * @return $this
+         */
+        public function setFormName($formName)
+        {
+            $this->formName = $formName;
+            $this->addField('OrveloName', $formName);
+
+            return $this;
+        }
+
+        /**
+         * @since 1.0
+         * @return string
+         */
+        public function getOrveloURL()
+        {
+            return $this->orveloURL;
+        }
+
+        /**
+         * @param string $orveloURL
+         *
+         * @since 1.0
+         * @return $this
+         */
+        public function setOrveloURL($orveloURL)
+        {
+            $this->orveloURL = $orveloURL;
+
+            return $this;
+        }
+
+        /**
+         * @since 1.0
+         * @return string
+         */
+        public function getVid()
+        {
+            return $this->vid;
+        }
+
+        /**
+         * @param string $vid
+         *
+         * @since 1.0
+         * @return $this
+         */
+        public function setVid($vid)
+        {
+            $this->vid = $vid;
+            $this->addField('vid', $this->getVid());
+
+            return $this;
+        }
+
+        /**
+         * @since 1.0
+         * @return string
+         */
+        public function getChannelHash()
+        {
+            return $this->channelHash;
+        }
+
+        /**
+         * @since 1.0
+         * @param string $channelHash
+         */
+        public function setChannelHash($channelHash)
+        {
+            $this->channelHash = $channelHash;
+            $this->addField('channel_hash', $channelHash);
+        }
+
+
+    }
